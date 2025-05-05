adef1c15d4bd6a0f16e85ef6aa67b3c3407778ae
samyeung <singtammy2@gmail.com>
2025-05-05 17:01:48 +0100

    add user

---
+++ backend/src/common/interceptor/serialize/serialize.interceptor.ts
@@ -0,0 +1,22 @@
+import {CallHandler, ExecutionContext, Injectable, NestInterceptor, UseInterceptors} from '@nestjs/common';
+import {map, Observable} from 'rxjs';
+import {plainToInstance} from "class-transformer";
+
+interface ClassConstructor{
+    new(...args: any[]):{};
+}
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
+export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: ClassConstructor) {
+    }
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        return next.handle().pipe(
+            map((data: ClassConstructor) => (plainToInstance(this.dto, data, {
+                excludeExtraneousValues: true
+            }))),
+        );
+    }
+}
---
+++ backend/src/common/middleware/logger.middleware.ts
@@ -0,0 +1,14 @@
+import {Injectable, Logger, NestMiddleware} from '@nestjs/common';
+import {Request,Response} from "express";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware<Request,Response> {
+  private readonly logger = new Logger(LoggerMiddleware.name, { timestamp: true });
+
+  use(req: Request, res: Response, next: (error?: any) => void): any {
+      this.logger.log(`[${req.method}][url] - ${req.originalUrl}`);
+      this.logger.log(`[${req.method}][Header] - ${JSON.stringify(req.headers)}`);
+      this.logger.log(`[${req.method}][Request] - ${JSON.stringify(req.body)}`);
+      next();
+  }
+}
