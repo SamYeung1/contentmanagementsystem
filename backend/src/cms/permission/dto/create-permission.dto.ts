641d0936206b5c2c866ca5b6da23e8f3cc765e1e
samyeung <singtammy2@gmail.com>
2026-01-07 12:11:59 +0000

    add regex CREATE/UPDATE/DELETE/READ/*@abc

--- backend/src/cms/permission/dto/create-permission.dto.ts
+++ backend/src/cms/permission/dto/create-permission.dto.ts
@@ -1,7 +1,10 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, Matches } from 'class-validator';
 
 export class CreatePermissionDto {
   @IsNotEmpty()
+  @Matches(/^(CREATE|UPDATE|DELETE|READ|\*)@([a-zA-Z0-9]+)$/,{
+    message:"Please enter a valid action [i'e: CREATE/UPDATE/DELETE/READ/*@abc]",
+  })
   action: string;
   @IsNotEmpty()
   name: string;
